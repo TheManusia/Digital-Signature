928d6053e20ddd989c80035546c07d738829420d
Ian <minecraftindo0@gmail.com>
2021-04-16 12:38:26 +0700

    Enable PDF Reader

--- app/src/main/java/xyz/themanusia/digitalsignature/ui/home/HomeActivity.java
+++ app/src/main/java/xyz/themanusia/digitalsignature/ui/home/HomeActivity.java
@@ -1,19 +1,24 @@
 package xyz.themanusia.digitalsignature.ui.home;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import xyz.themanusia.digitalsignature.R;
 import xyz.themanusia.digitalsignature.databinding.ActivityHomeBinding;
+import xyz.themanusia.digitalsignature.ui.pdf.PdfActivity;
 import xyz.themanusia.digitalsignature.ui.signature.SignatureActivity;
 
 public class HomeActivity extends AppCompatActivity {
     private ActivityHomeBinding binding;
-    //    private boolean isOpen = false;
+    private boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,60 +35,59 @@ public class HomeActivity extends AppCompatActivity {
             binding.rvSignature.setHasFixedSize(true);
         });
 
-//        binding.fbOpen.setOnClickListener(view -> {
-//            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//            intent.setType("application/pdf");
-//            startActivityForResult(intent, 1);
-//        });
+        binding.fbOpen.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("application/pdf");
+            startActivityForResult(intent, 1);
+        });
 
-//        binding.fbNew.setOnClickListener(view ->
-//                startActivity(new Intent(this, SignatureActivity.class)));
-
-        binding.fbTrigger.setOnClickListener(view -> //{
+        binding.fbNew.setOnClickListener(view ->
                 startActivity(new Intent(this, SignatureActivity.class)));
-//            if (isOpen)
-//                closeFbMenu();
-//            else
-//                showFbMenu();
-//      }
+
+        binding.fbTrigger.setOnClickListener(view -> {
+            if (isOpen)
+                closeFbMenu();
+            else
+                showFbMenu();
+        });
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (resultCode == RESULT_OK) {
-//            if (data != null) {
-//                Uri pdfUri = data.getData();
-//                Intent intent = new Intent(this, PdfActivity.class);
-//                intent.putExtra(PdfActivity.PDF_URI, pdfUri.toString());
-//                startActivity(intent);
-//            } else {
-//                Toast.makeText(this, "Can't Open File", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri pdfUri = data.getData();
+                Intent intent = new Intent(this, PdfActivity.class);
+                intent.putExtra(PdfActivity.PDF_URI, pdfUri.toString());
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Can't Open File", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
-//    @Override
-//    public void onBackPressed() {
-//        if (isOpen)
-//            closeFbMenu();
-//        else
-//            super.onBackPressed();
-//    }
+    @Override
+    public void onBackPressed() {
+        if (isOpen)
+            closeFbMenu();
+        else
+            super.onBackPressed();
+    }
 
-//    private void showFbMenu() {
-//        isOpen = true;
-//        binding.fbOpen.animate().translationY(-getResources().getDimension(R.dimen.standard_60)).withStartAction(() -> binding.fbOpen.setVisibility(View.VISIBLE));
-//        binding.tvOpen.animate().translationY(-getResources().getDimension(R.dimen.standard_60)).withStartAction(() -> binding.fbNew.setVisibility(View.VISIBLE));
-//        binding.fbNew.animate().translationY(-getResources().getDimension(R.dimen.standard_110)).withStartAction(() -> binding.fbNew.setVisibility(View.VISIBLE));
-//        binding.tvNew.animate().translationY(-getResources().getDimension(R.dimen.standard_110)).withStartAction(() -> binding.tvNew.setVisibility(View.VISIBLE));
-//    }
+    private void showFbMenu() {
+        isOpen = true;
+        binding.fbOpen.animate().translationY(-getResources().getDimension(R.dimen.standard_60)).withStartAction(() -> binding.fbOpen.setVisibility(View.VISIBLE));
+        binding.tvOpen.animate().translationY(-getResources().getDimension(R.dimen.standard_60)).withStartAction(() -> binding.tvOpen.setVisibility(View.VISIBLE));
+        binding.fbNew.animate().translationY(-getResources().getDimension(R.dimen.standard_110)).withStartAction(() -> binding.fbNew.setVisibility(View.VISIBLE));
+        binding.tvNew.animate().translationY(-getResources().getDimension(R.dimen.standard_110)).withStartAction(() -> binding.tvNew.setVisibility(View.VISIBLE));
+    }
 
-//    private void closeFbMenu() {
-//        isOpen = false;
-//        binding.fbNew.animate().translationY(0).withEndAction(() -> binding.fbNew.setVisibility(View.GONE));
-//        binding.tvNew.animate().translationY(0).withEndAction(() -> binding.tvNew.setVisibility(View.GONE));
-//        binding.fbOpen.animate().translationY(0).withEndAction(() -> binding.fbOpen.setVisibility(View.GONE));
-//        binding.tvOpen.animate().translationY(0).withEndAction(() -> binding.tvOpen.setVisibility(View.GONE));
-//    }
+    private void closeFbMenu() {
+        isOpen = false;
+        binding.fbNew.animate().translationY(0).withEndAction(() -> binding.fbNew.setVisibility(View.GONE));
+        binding.tvNew.animate().translationY(0).withEndAction(() -> binding.tvNew.setVisibility(View.GONE));
+        binding.fbOpen.animate().translationY(0).withEndAction(() -> binding.fbOpen.setVisibility(View.GONE));
+        binding.tvOpen.animate().translationY(0).withEndAction(() -> binding.tvOpen.setVisibility(View.GONE));
+    }
 }
